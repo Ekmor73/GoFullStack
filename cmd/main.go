0e678dc13e7a39a8292eeb6dcb47ff7a4fdcc31c
Alex <ekmor73@gmail.com>
2025-04-29 06:27:47 +0400

    настройка .env, .gitignor, config

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"go-fiber/config"
 	"go-fiber/internal/home"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.Init()
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
 	// Создаем новый экземпляр Fiber
 	app := fiber.New()
 
