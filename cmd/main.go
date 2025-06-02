147076397efae8365d3467febd2a49fa1490a3d2
Alex <ekmor73@gmail.com>
2025-06-02 04:45:23 +0400

    Middleware

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"go-fiber/internal/vacancy"
 	"go-fiber/pkg/database"
 	"go-fiber/pkg/logger"
+	"go-fiber/pkg/middleware"
 	"time"
 
 	"github.com/gofiber/contrib/fiberzerolog"
@@ -47,6 +48,7 @@ func main() {
 	store := session.New(session.Config{
 		Storage: storage,
 	})
+	app.Use(middleware.AuthMiddleware(store))
 
 	// Repositories
 	vacancyRepo := vacancy.NewVacancyRepository(dbpool, customLogger)
